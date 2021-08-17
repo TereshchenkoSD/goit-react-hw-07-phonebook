f49b4ee888fddcff99175fcf4cce58c4bcf3ac18
Sergey Tereshchenko <serghh1988@gmail.com>
2021-08-17 23:22:19 +0300

    Закомментировал весь код пакета redux-persist и все что связано с работой с LocalStorage.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
-import storeObject from './redux/store';
+import store from './redux/store';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <PersistGate loading={null} persistor={storeObject.persistor}>
-      <Provider store={storeObject.store}>
-        <App />
-      </Provider>
-    </PersistGate>
+    {/* <PersistGate loading={null} persistor={storeObject.persistor}> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+    {/* </PersistGate> */}
   </React.StrictMode>,
   document.querySelector('#root'),
 );
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,39 +1,45 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 import { rootReducer } from '../reducers';
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
+// const persistConfig = {
+//   key: 'contacts',
+//   storage,
+//   blacklist: ['filter'],
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const store = configureStore({
+//   reducer: rootReducer,
+//   devTools: process.env.NODE_ENV === 'development',
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }).concat(logger),
+// });
 
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   devTools: process.env.NODE_ENV === 'development',
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }).concat(logger),
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 });
 
-const persistor = persistStore(store);
+// const persistor = persistStore(store);
 
-const storeObject = { store, persistor };
+// const storeObject = { store, persistor };
 
-export default storeObject;
+export default store;
